40bcaccfe5cc80e35205cbb1e43cd3d30f4820a7
codemonkey110 <7145055+codemonkey110@users.noreply.github.com>
2024-11-03 03:08:59 +0000

    1. Updated Callback Function Parameters based on (https://python.langchain.com/v0.1/docs/modules/callbacks/) 2. Updated code to be more verbose to facilitate observability 3. Included Disclaimer as required for submission

--- InfoSynth.py
+++ InfoSynth.py
@@ -14,6 +14,13 @@ if not check_password():
 
 st.write("# Welcome to the InfoSynth (proof of concept)! 👋")
 
+with st.expander("Disclaimer"):
+    st.write('''
+        IMPORTANT NOTICE: This web application is developed as a proof-of-concept prototype. The information provided here is NOT intended for actual usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters.
+        Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output.
+        Always consult with qualified professionals for accurate and personalized advice.
+    ''')
+
 st.sidebar.success("Select a page above.")
 
 st.markdown(
