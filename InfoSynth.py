b009860b9608f6ea2eb0b3cd09b1435af4e8585b
codemonkey110 <7145055+codemonkey110@users.noreply.github.com>
2024-11-06 00:45:37 +0000

    Further Updates for Submission

--- InfoSynth.py
+++ InfoSynth.py
@@ -20,23 +20,3 @@ with st.expander("Disclaimer"):
         Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output.
         Always consult with qualified professionals for accurate and personalized advice.
     ''')
-
-st.sidebar.success("Select a page above.")
-
-st.markdown(
-    """
-    Streamlit is an open-source app framework built specifically for
-    Machine Learning and Data Science projects.
-    **👈 Select a demo from the sidebar** to see some examples
-    of what Streamlit can do!
-    ### Want to learn more?
-    - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in our [community
-        forums](https://discuss.streamlit.io)
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-"""
-)
